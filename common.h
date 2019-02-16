a48b1c3b76e80f5ff875320efacd20d2a25fbe2d
Daniel <den218den@gmail.com>
2019-02-16 23:31:44 +0300

    initial

---
+++ common.h
@@ -0,0 +1,14 @@
+typedef struct _test_struct{
+
+    char name[60];
+    char surname[60];
+    unsigned int age;
+    char group[60];
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+    char result[128];
+
+} result_struct_t;
